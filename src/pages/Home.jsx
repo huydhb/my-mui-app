24986f51f1d91481dc08e972658f76f4109291de
huydhb <danghoangbaohuyc5@gmail.com>
2025-10-22 21:35:46 +0700

    First commit - MUI app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+      <Typography variant="h6" color="secondary">
+        Đây là trang Home 🏠
+      </Typography>
+    </div>
+  );
+}
